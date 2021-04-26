92cf035b07fb10b732b1023b8cd27c568eeb4d72
lilonghe <lilonghe@outlook.com>
2021-04-26 10:54:00 +0800

    add: generateImageByString

---
+++ generateImage.js
@@ -0,0 +1,29 @@
+const getColorByName = (name) => {
+    return '#' + (name.charCodeAt(0).toString().slice(0, 6).padEnd(6, '0'));
+};
+
+export function generateImageByString(str, userOptions = {}) {
+    let options = {
+        w: 64,
+        h: 64,
+        ...userOptions,
+    }
+    let name = str;
+    let canvas = document.createElement('canvas');
+    canvas.setAttribute('width', options.w);
+    canvas.setAttribute('height', options.w);
+    canvas.style.border = '1px solid #CCC';
+
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = getColorByName(name);
+    ctx.fillRect(0, 0, 64, 64);
+    ctx.font = `${options.w * 0.7}px Arial`;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = '#FFF';
+    ctx.fillText(name[0], options.w / 2, options.h / 2 + options.h * 0.07);
+
+    let base64Img = canvas.toDataURL('image/png');
+    return base64Img;
+}
+
