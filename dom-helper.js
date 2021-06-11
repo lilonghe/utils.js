25342462ffc354160b7d3915b10c1c6a8ad087f4
longhe.li <lilonghe@outlook.com>
2021-06-11 17:48:56 +0800

    feat: scrollSelector  is optional

--- dom-helper.js
+++ dom-helper.js
@@ -2,7 +2,7 @@ export function checkIsIntoView(targetSelector, scrollSelector, offsetSize = 0) 
 	let targetEle = document.querySelector(targetSelector);
 	let canViewTop =  targetEle.offsetTop;
 
-	let scrollEle = document.querySelector(scrollSelector);
+	let scrollEle =  scrollSelector ? document.querySelector(scrollSelector) : targetEle.offsetParent;
 	let offsetBottom = scrollEle.scrollTop + scrollEle.clientHeight;
 
 	if (offsetBottom - canViewTop + offsetSize >= 0) {
