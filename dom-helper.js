053736d7a77bc3b6d5d4c9209b9a25fde9e2a1e5
longhe.li <lilonghe@outlook.com>
2021-06-16 10:22:57 +0800

    format code

--- dom-helper.js
+++ dom-helper.js
@@ -1,15 +1,15 @@
 export function checkIsIntoView(targetSelector, scrollSelector, offsetSize = 0) {
-	let targetEle = document.querySelector(targetSelector);
-	let canViewTop =  targetEle.offsetTop;
+    let targetEle = document.querySelector(targetSelector);
+    let canViewTop = targetEle.offsetTop;
 
-	let scrollEle =  scrollSelector ? document.querySelector(scrollSelector) : targetEle.offsetParent;
-	let offsetBottom = scrollEle.scrollTop + scrollEle.clientHeight;
+    let scrollEle = scrollSelector ? document.querySelector(scrollSelector) : targetEle.offsetParent;
+    let offsetBottom = scrollEle.scrollTop + scrollEle.clientHeight;
 
-	if (offsetBottom - canViewTop + offsetSize >= 0) {
-		return true;
-	} else {
-		return false;
-	}
+    if (offsetBottom - canViewTop + offsetSize >= 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 export function onIntoView({ targetSelector, scrollSelector, offsetSize = 0, thresholds: 1.0, callback}) {
